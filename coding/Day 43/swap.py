042207e6cbfa8de9eff4007aa9fbb0fa7a96deac
Veekshithshetty <veekshithshetty99@gmail.com>
2020-07-18 17:06:47 +0530

    Day 43

---
+++ coding/Day 43/swap.py
@@ -0,0 +1,25 @@
+Shapedef swapPositions(list, pos1, pos2): 
+
+ 
+
+Shapelist[pos1], list[pos2] = list[pos2], list[pos1] 
+
+ 
+
+Shapereturn list 
+
+ 
+
+Shape# Driver function 
+
+ 
+
+ShapeList = [23, 65, 19, 90] 
+
+ 
+
+Shapepos1, pos2 = 1, 3 
+
+ 
+
+Shapeprint(swapPositions(List, pos1-1, pos2-1)) 
